488c59d88059adaf8d8c045204df9bc31314d574
Hiren Ramoliya <hiren086.rejoice@gmail.com>
2021-11-08 15:11:50 +0530

    change message, modify api of get question and refactore api of verify email

--- src/api/question/get-question.js
+++ src/api/question/get-question.js
@@ -30,6 +30,7 @@ module.exports = exports = {
       };
     }
     console.log("Criteria---->", criteria);
+    const { filter } = req.body;
     try {
       req.query.page = req.query.page ? req.query.page : 1;
       let page = parseInt(req.query.page);
@@ -44,7 +45,78 @@ module.exports = exports = {
         })
         .skip(skip)
         .limit(limit);
-
+      console.log("useruser", user.accepted);
+      let findConection = await global.models.GLOBAL.QUESTION.find({
+        senderId: user._id,
+      });
+      const pandingIdExist = (id) => {
+        return findConection.length
+          ? findConection.receiverId.some(function (el) {
+              return el.toString() === id.toString();
+            })
+          : false;
+      };
+      const conectIdExist = (id) => {
+        return user.accepted.length
+          ? user.accepted.some(function (el) {
+              return el.toString() === id.toString();
+            })
+          : false;
+      };
+      let questionDetais = [];
+      for (let i = 0; i < question.length; i++) {
+        console.log("question.length---->>", i);
+        console.log("Id--->", question[i]);
+        if (conectIdExist(question[i].createdBy?._id)) {
+          console.log("Id--->", question[i].createdBy?._id);
+          const questionDetaisObj = {
+            _id: question[i]._id,
+            displayProfile: question[i].displayProfile,
+            allowConnectionRequest: question[i].allowConnectionRequest,
+            view: question[i].view,
+            response: question[i].response,
+            status: question[i].status,
+            question: question[i].question,
+            filter: question[i].filter,
+            createdAt: question[i].createdAt,
+            createdBy: question[i].createdBy,
+            isFriend: true,
+          };
+          questionDetais.push(questionDetaisObj);
+        } else if (pandingIdExist(question[i].createdBy?._id)) {
+          console.log("Id--->", question[i].createdBy?._id);
+          const questionDetaisObj = {
+            _id: question[i]._id,
+            displayProfile: question[i].displayProfile,
+            allowConnectionRequest: question[i].allowConnectionRequest,
+            view: question[i].view,
+            response: question[i].response,
+            status: question[i].status,
+            question: question[i].question,
+            filter: question[i].filter,
+            createdAt: question[i].createdAt,
+            createdBy: question[i].createdBy,
+            isFriend: panding,
+          };
+          questionDetais.push(questionDetaisObj);
+        } else {
+          console.log("Id--->", question[i].createdBy?._id);
+          const questionDetaisObj = {
+            _id: question[i]._id,
+            displayProfile: question[i].displayProfile,
+            allowConnectionRequest: question[i].allowConnectionRequest,
+            view: question[i].view,
+            response: question[i].response,
+            status: question[i].status,
+            question: question[i].question,
+            filter: question[i].filter,
+            createdAt: question[i].createdAt,
+            createdBy: question[i].createdBy,
+            isFriend: false,
+          };
+          questionDetais.push(questionDetaisObj);
+        }
+      }
       // today's Questions Count
       let TodayQuestion = await global.models.GLOBAL.QUESTION.find({
         createdAt: {
@@ -69,7 +141,7 @@ module.exports = exports = {
         result: 0,
         message: messages.SUCCESS,
         payload: {
-          question,
+          questionDetais,
           count: question.length,
           todaysCount: TodayQuestion.length,
           profileaccess: QuestionProfileAccess.length,
@@ -82,6 +154,48 @@ module.exports = exports = {
       res
         .status(enums.HTTP_CODES.OK)
         .json(utils.createResponseObject(data4createResponseObject));
+      if (filter) {
+        console.log(
+          "Filter--->",
+          await global.models.GLOBAL.QUESTION.find(
+            { criteria },
+            { filter: { $in: { filterId: [...filter] } } }
+          )
+        );
+        req.query.page = req.query.page ? req.query.page : 1;
+        let page = parseInt(req.query.page);
+        req.query.limit = req.query.limit ? req.query.limit : 10;
+        let limit = parseInt(req.query.limit);
+        let skip = (parseInt(req.query.page) - 1) * limit;
+        let question = await global.models.GLOBAL.QUESTION.find(
+          { criteria },
+          { filter: { $in: { filterId: [...filter] } } }
+        )
+          .populate({
+            path: "createdBy",
+            model: "user",
+            select: "name",
+          })
+          .skip(skip)
+          .limit(limit);
+
+        console.log("Question by filter---->", question);
+        const data4createResponseObject = {
+          req: req,
+          result: 0,
+          message: messages.SUCCESS,
+          payload: {
+            question,
+            count: question.length,
+            page,
+            limit,
+          },
+          logPayload: false,
+        };
+        res
+          .status(enums.HTTP_CODES.OK)
+          .json(utils.createResponseObject(data4createResponseObject));
+      }
     } catch (error) {
       logger.error(
         `${req.originalUrl} - Error encountered: ${error.message}\n${error.stack}`
--- src/api/user/verify-email.js
+++ src/api/user/verify-email.js
@@ -18,7 +18,7 @@ module.exports = exports = {
     const { email } = req.body;
 
     let code = Math.floor(Math.random() * (999999 - 100000 + 1) + 100000);
-    console.log("COde---------->>>", code);
+    console.log("Code---------->>>", code);
     // const locale = utils.getLocale(req);
     let entry;
     // If codes already exists for this email in the database delete them
@@ -62,70 +62,86 @@ module.exports = exports = {
     }
 
     // When USE_TEST_PIN is true (config.json)
-
-    if (String(findUser.email) === String(email)) {
-      console.log("MAIL SENDING");
-      let transporter = nodemailer.createTransport({
-        service: "gmail",
-        host: "smtp.gmail.com",
-        port: 587,
-        secure: false,
-        auth: {
-          user: process.env.EMAIL,
-          pass: process.env.PASSWORD,
-        },
-      });
-      let info = await transporter.sendMail({
-        from: process.env.EMAIL,
-        to: email,
-        subject: "Account Verification Code",
-        html: `Email :- ${email} <br /><br />
-        One Time Code:- ${code}`,
-      });
-      console.log("Message sent: %s", info.messageId);
-      if (config.MONGODB.GLOBAL.USE_TEST_PIN) {
-        // If (dummyAccount) {
-        code = code;
-        console.log("CODE NEw----=======>>>>>", code);
-        // Save the code in database
-        entry = global.models.GLOBAL.CODE_VERIFICATION({
-          email: email,
-          code: code,
-          date: Date.now(),
-          expirationDate: Date.now() + 300 * 1000,
-          failedAttempts: 0,
+    try {
+      if (String(findUser.email) === String(email)) {
+        console.log("MAIL SENDING");
+        let transporter = nodemailer.createTransport({
+          service: "gmail",
+          host: "smtp.gmail.com",
+          port: 587,
+          secure: false,
+          auth: {
+            user: process.env.EMAIL,
+            pass: process.env.PASSWORD,
+          },
         });
+        let info = await transporter.sendMail({
+          from: process.env.EMAIL,
+          to: email,
+          subject: "Account Verification Code",
+          html: `Email :- ${email} <br /><br />
+        One Time Code:- ${code}`,
+        });
+        console.log("Message sent: %s", info.messageId);
 
-        logger.info("/verify-email - Saving verification-code in database");
-        try {
-          await entry.save();
-        } catch (error) {
-          logger.error(
-            `/verify-email - Error while saving code in database: ${error.message}\n${error.stack}`
-          );
+        if (config.MONGODB.GLOBAL.USE_TEST_PIN) {
+          // If (dummyAccount) {
+          code = code;
+          console.log("CODE NEw----=======>>>>>", code);
+          // Save the code in database
+          entry = global.models.GLOBAL.CODE_VERIFICATION({
+            email: email,
+            code: code,
+            date: Date.now(),
+            expirationDate: Date.now() + 300 * 1000,
+            failedAttempts: 0,
+          });
+
+          logger.info("/verify-email - Saving verification-code in database");
+          try {
+            await entry.save();
+          } catch (error) {
+            logger.error(
+              `/verify-email - Error while saving code in database: ${error.message}\n${error.stack}`
+            );
+            const data4createResponseObject = {
+              req: req,
+              result: -1,
+              message: messages.FAILED_VERIFICATION,
+              payload: {},
+              logPayload: false,
+            };
+            return res
+              .status(enums.HTTP_CODES.INTERNAL_SERVER_ERROR)
+              .json(utils.createResponseObject(data4createResponseObject));
+          }
           const data4createResponseObject = {
             req: req,
-            result: -1,
-            message: messages.FAILED_VERIFICATION,
+            result: 0,
+            message:
+              "[USE_TEST_PIN = False] Mail was sent out to the Email Address.",
             payload: {},
             logPayload: false,
           };
           return res
-            .status(enums.HTTP_CODES.INTERNAL_SERVER_ERROR)
+            .status(enums.HTTP_CODES.OK)
             .json(utils.createResponseObject(data4createResponseObject));
         }
-        const data4createResponseObject = {
-          req: req,
-          result: 0,
-          message:
-            "[USE_TEST_PIN = False] Mail was sent out to the Email Address.",
-          payload: {},
-          logPayload: false,
-        };
-        return res
-          .status(enums.HTTP_CODES.OK)
-          .json(utils.createResponseObject(data4createResponseObject));
       }
+    } catch (error) {
+      logger.error(
+        `${req.originalUrl} - Error while sending Mail : ${error.message}\n${error.stack}`
+      );
+      const data4createResponseObject = {
+        req: req,
+        result: -1,
+        message: messages.GENERAL,
+        payload: {},
+        logPayload: false,
+      };
+      return res
+        .status(enums.HTTP_CODES.INTERNAL_SERVER_ERROR)
+        .json(utils.createResponseObject(data4createResponseObject));
     }
   },
 };
