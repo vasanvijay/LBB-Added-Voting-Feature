6d9fbbe5b825dddfd69cf39e3c2bf5c731d3fbe5
Hiren Ramoliya <hiren086.rejoice@gmail.com>
2021-12-11 11:47:20 +0530

    Fix issue of Verification form

--- src/api/user/user-data.js
+++ src/api/user/user-data.js
@@ -7,7 +7,6 @@ const utils = require("../../utils");
 module.exports = exports = {
   // route validation
   validation: Joi.object({
-    profileImage: Joi.string().allow(""),
     email: Joi.string().required(),
     organizationName: Joi.string().required(),
     currentRole: Joi.string().required(),
@@ -22,7 +21,6 @@ module.exports = exports = {
 
   handler: async (req, res) => {
     const {
-      profileImage,
       email,
       organizationName,
       currentRole,
@@ -59,48 +57,88 @@ module.exports = exports = {
         $or: [{ email: { $eq: email } }],
       });
       if (findUser) {
-        const image = await utils.uploadBase(profileImage, findUser._id);
-        let fillForm = await global.models.GLOBAL.USER.findOneAndUpdate(
-          { email: email },
+        const ImagesList = await global.models.GLOBAL.LEGENDS.aggregate([
           {
-            $set: {
-              profileImage: image,
-              organizationName: organizationName,
-              currentRole: currentRole,
-              region: region,
-              organizationEmail: organizationEmail,
-              linkedinProfile: linkedinProfile,
-              organizationWebsite: organizationWebsite,
-              otherLink: otherLink,
-              howDidFind: howDidFind,
-              subject: subject,
-              formFilled: true,
-            },
+            $match: { legendsName: { $in: subject } },
           },
-          { new: true }
-        );
-        if (!fillForm) {
-          const data4createResponseObject = {
-            req: req,
-            result: -1,
-            message: messages.GENERAL,
-            payload: {},
-            logPayload: false,
-          };
-          return res
-            .status(enums.HTTP_CODES.BAD_REQUEST)
-            .json(utils.createResponseObject(data4createResponseObject));
+          {
+            $group: { _id: "$legendsIcon" },
+          },
+        ]);
+
+        console.log("LIST LENGHT", ImagesList?.length);
+
+        if (ImagesList.length !== 4) {
+          return res.status(400).json({
+            message: "Please provide 4 images",
+          });
         } else {
-          const data4createResponseObject = {
-            req: req,
-            result: 0,
-            message: messages.ITEM_UPDATED,
-            payload: { fillForm },
-            logPayload: false,
-          };
-          return res
-            .status(enums.HTTP_CODES.OK)
-            .json(utils.createResponseObject(data4createResponseObject));
+          const canvasWidth = 1024;
+          const canvasHeight = 1024;
+          //load canvas
+          const myCanvas = createCanvas(canvasWidth, canvasHeight, "PNG");
+          const context = myCanvas.getContext("2d");
+
+          //load images && draw image in existing canvas
+
+          let imageWidth = 0;
+          let imageHeight = 0;
+          for (i = 0; i < ImagesList.length; i++) {
+            await loadImage(ImagesList[i]._id).then(async (image) => {
+              await context.drawImage(image, imageWidth, imageHeight);
+              imageHeight =
+                canvasHeight > imageHeight * 2 ? imageWidth : imageHeight;
+              imageWidth =
+                canvasWidth > imageWidth * 2 ? 0.5 * canvasHeight : 0;
+            });
+          }
+          let image = await utils.uploadBase(
+            myCanvas.toDataURL(),
+            findUser._id
+          );
+          let fillForm = await global.models.GLOBAL.USER.findOneAndUpdate(
+            { email: email },
+            {
+              $set: {
+                profileImage: image,
+                organizationName: organizationName,
+                currentRole: currentRole,
+                region: region,
+                organizationEmail: organizationEmail,
+                linkedinProfile: linkedinProfile,
+                organizationWebsite: organizationWebsite,
+                otherLink: otherLink,
+                howDidFind: howDidFind,
+                subject: subject,
+                formFilled: true,
+              },
+            },
+            { new: true }
+          );
+
+          if (!fillForm) {
+            const data4createResponseObject = {
+              req: req,
+              result: -1,
+              message: messages.GENERAL,
+              payload: {},
+              logPayload: false,
+            };
+            return res
+              .status(enums.HTTP_CODES.BAD_REQUEST)
+              .json(utils.createResponseObject(data4createResponseObject));
+          } else {
+            const data4createResponseObject = {
+              req: req,
+              result: 0,
+              message: messages.ITEM_UPDATED,
+              payload: { fillForm },
+              logPayload: false,
+            };
+            return res
+              .status(enums.HTTP_CODES.OK)
+              .json(utils.createResponseObject(data4createResponseObject));
+          }
         }
       }
     } catch (error) {
