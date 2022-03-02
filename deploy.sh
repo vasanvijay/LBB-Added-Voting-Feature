39240d92013733da0f82d91008735f59a3fa0004
ankit067.rejoice <ankit067.rejoice@gmail.com>
2022-03-02 11:06:44 +0100

    hotfix: md5 require error

---
+++ deploy.sh
@@ -0,0 +1,4 @@
+cd /home/admin/client/leaderbridge/leaderbridge-backend
+git pull
+npm install
+pm2 start app.yml
--- src/utils.js
+++ src/utils.js
@@ -7,7 +7,7 @@ const { default: jwtDecode } = require("jwt-decode");
 const multer = require("multer");
 const multerS3 = require("multer-s3");
 var AWS = require("aws-sdk");
-
+const md5 = require("md5");
 const { s3Config } = require("./middlewares/config");
 AWS.config.update({
   accessKeyId: s3Config.clientId,
