1d3c86332505f30074767b095de5ecd3e7a8cc64
vijayvasan <vijay085.rejoice@gmail.com>
2022-06-07 12:48:36 +0530

    fixed issue for send mail

--- src/api/user/send-mail.js
+++ src/api/user/send-mail.js
@@ -64,18 +64,115 @@ module.exports = exports = {
           },
         });
 
-        for (let i = 0; i < FFF.length; i++) {
+        for (let i = 0; i < findUser.length; i++) {
           await global.models.GLOBAL.USER.findByIdAndUpdate(
-            { _id: FFF[i]._id },
+            { _id: findUser[i]._id },
             { $set: { isSubmit: false } }
           );
-          html = html.replace(/\[\[name\]\]/g, FFF[i].email);
-          html = html.replace(/\[\[uid\]\]/g, FFF[i]._id);
+          html = html.replace(/\[\[name\]\]/g, findUser[i].email);
+          html = html.replace(/\[\[uid\]\]/g, findUser[i]._id);
           let info = await transporter.sendMail({
             from: process.env.EMAIL,
-            to: FFF[i].email,
+            to: findUser[i].email,
             subject: "LeaderBridge | OTP To Verify Your Email",
-            html: `${html}`,
+            html: `<!DOCTYPE html>
+            <html lang="en">
+            
+            <head>
+                <meta charset="UTF-8">
+                <meta name="viewport" content="width=device-width, initial-scale=1.0">
+                <link href="https://fonts.googleapis.com/css2?family=Ubuntu:wght@400;500;700&display=swap" rel="stylesheet">
+                <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css"> 
+                <script src="https://kit.fontawesome.com/b7ecb94de9.js" crossorigin="anonymous"></script>
+      
+                </head>
+            <style>
+                body {
+                    font-family: 'Ubuntu', sans-serif;
+                    background-color: #f5f5f5;
+                }
+            
+                * {
+                    box-sizing: border-box;
+                }
+            
+                p:last-child {
+                    margin-top: 0;
+                }
+            
+                img {
+                    max-width: 100%;
+                }
+            </style>
+            
+            <body style="margin: 0; padding: 0;">
+            
+                <table cellpadding="0" cellspacing="0" width="100%">
+                    <tr>
+                        <td style="padding: 20px 0 30px 0;">
+                            <table align="center" cellpadding="0" cellspacing="0" width="600" style=" border-collapse: collapse; border: 1px solid #ececec; background-color: #fff;">
+                                <tr>
+                                    <td align="center" style="position: relative;">
+                                        <div
+                                        class="company-logo-align"
+                                        style=" padding: 2rem 2rem 1rem 2rem; display: flex; align-items: center; justify-content: center; margin: 0 auto;"
+                                        align="center">
+                                            <img  src="https://leadersbridge.s3.amazonaws.com/site-assets/logo.png" style= "margin:0 auto; width: 80px;height: 80px;cursor: pointer;"/>
+                                        </div>
+                                    </td>
+                                </tr>
+                                <tr>
+                                    <td>
+                                        <div class="user-information" 
+                                        style="padding: 25px; background-color: #021f4c; width: 91.6%;"
+                                        >
+                                        <p align="center" style="color: #fff; font-size: 30px; font-weight: 500; margin: 0 0 1rem 0;">Welcome to LeaderBridge®</p>
+                                        <span align="center" style="display: block; font-size: 16px; color: #fff;">Thank you for signing up on LeaderBridge®</span>
+                                        </div>
+                                      
+                                    </td> 
+                                    <td></td>
+                                </tr>
+                                <tr>
+                                    <td style="padding: 3rem 2rem 2rem 2rem;">
+                                      <h6 align="center" style="color: #585d6a; font-size: 30px; ">Dear LeaderBridge Member,
+      </h6>
+                                      <p style="color: #585d6a; font-size: 14px; margin: 2.50rem 0 2rem 0;">I want to thank you for joining the LeaderBridge community and participating in our project.
+      </p>
+                                                                                <p style="color: #585d6a; font-size: 14px; margin: 2.50rem 0 2rem 0;">Based on what we have learned during the past two years, we are re-tooling the platform and focusing it on the founders of startups and early-stage companies.
+      
+      </p>
+      <p style="color: #585d6a; font-size: 14px; margin: 2.50rem 0 2rem 0;">To make this transition we will close access to the site on Sunday, June 12, 2022, and close all accounts.  If you are a founder and would like to stay on the new LeaderBridge, please <a href="https://leaderbridge.com/active/[[uid]]">click here</a> to choose to keep your account and we will let you know when the new version of LeaderBridge goes live.
+                                
+                                      <h6 style="font-size: 12px; color: #585d6a; margin: 0;  margin-top: 0;">Thank you again for your participation,</h6>
+                <h6 style="font-size: 30px; color: #585d6a; margin: 16px 0;">John</h6>
+                                    <p style="font-size: 14px; color: #585d6a;">John Behr</p>
+                                                                                                  <p style="font-size: 14px; color: #585d6a; margin: 0;  margin-top: 0;">Founder and CEO</p>
+                                    </td>
+                                </tr>
+                                <tr>
+                                    <td style="padding: 3rem 2rem 2rem 2rem;">
+                                      <p align="center" style="color: #585d6a; font-size: 14px; margin: 0;">
+                                        If you have any query, feel free to contact us at support@leaderbridge.com.
+                                      </p>
+      
+      
+                                      <p style="display: flex;align-items: center;" ><img src="https://img.icons8.com/material-rounded/24/000000/facebook-new.png"/><a  target="_blank" href="https://m.facebook.com/leaderbridge/?ref=py_c">Facebook</a></p> 
+                                      <p style="display: flex;align-items: center;" ><img src="https://img.icons8.com/material/24/000000/linkedin--v1.png"/><a  target="_blank" href="https://www.linkedin.com/company/leader-bridge">Linkedin</a></p>
+                                      <p style="display: flex;align-items: center;"><img src="https://img.icons8.com/material-rounded/24/000000/instagram-new.png"/><a  target="_blank" href="https://www.instagram.com/leaderbridge1/">Instagram</a></p>
+                                    </td>
+                                 
+                                </tr>
+                                
+                               
+      
+                            </table>
+                        </td>
+                    </tr>
+                </table>
+            </body>
+            
+            </html>`,
           });
         }
 
